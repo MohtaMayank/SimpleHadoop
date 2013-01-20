8553953130298040b195f816f54b10492b17d162
MohtaMayank <mmohta@andrew.cmu.edu>
2013-01-20 00:38:41 -0500

    First Change

--- cmu/cs/distsystems/hw1/HelloWorld.java
+++ cmu/cs/distsystems/hw1/HelloWorld.java
@@ -1,12 +1,25 @@
 package cmu.cs.distsystems.hw1;
 
+/**
+ * Dummy class to test git repo
+ * Steps to make a git repo:
+ * 1. Create a Java Project in eclipse
+ * 2. Team > Share (Create a new repo in the parent dir of workspace)
+ * 3. Add a .gitignore file with bin/
+ * 4. Make some change
+ * 5. Commit to the local repo.
+ * 6. Team > remote > push ... Push to remote repo (select repo by url)
+ * @author mayank
+ *
+ */
+
 public class HelloWorld {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("First Change");
 
 	}
 
